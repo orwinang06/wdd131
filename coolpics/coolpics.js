f925001d5cd34ddaa3b74cf25fbd1291efce95b8
orwinang06 <angelorwin06@gmail.com>
2025-02-02 00:14:35 -0700

    yes?

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,3 +1,10 @@
+const menuButton = document.querySelector(".menu-button");
+function toggleMenu() {
+  const menu = document.querySelector(".menu");
+  menu.classList.toggle("hide");
+}
+
+menuButton.addEventListener("click", toggleMenu);
 
 
 function viewerTemplate(path, text){
