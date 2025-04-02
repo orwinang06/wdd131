2b41d3adc71bf2745eb260936820f8fcc9389256
orwinang06 <angelorwin06@gmail.com>
2025-04-02 09:55:03 -0600

    so far

---
+++ site_final/site.js
@@ -0,0 +1,113 @@
+const blog = [
+    {
+		id: 1,
+		title: "Airport Tips",
+		date: "March 20, 2025",
+        sub_1: "Arrive Early",
+		description_1:
+            "Busy times at the airport are often summer breaks, spring break, and holidays. Many like to try to avoid these days, but when going with a family, you may have to work around school schedules, which means spring break, and summer break for many. Holidays are always busy, people trying to go home to family, families traveling to see extended family. Plan an extra hour during these times, security lines may be extra long, even for precheck. I for a normal flight outside of that time like to plan at least 2 hours before the flight to be at the airport, but for busy times arrive closer to 3 hours earlier. If you are traveling international most recommend you arrive 3 hour early anyways, so if your traveling with your group internationally during busy times, maybe aim for four hours early. Save yourself the stress of wondering if you are going to get through security and to your gate on time.",
+        sub_2: "ID",
+        description2:
+            "Make sure you have valid and the right kind of ID. The United States is changing standard on this May 7th, 2025, so look up what the valid ID for to make sure you have valid ID for US citizens for domestic flights. If you are traveling internationally, make sure you have a valid passport.",
+        sub_3: "Electronics",
+        description3:
+            "I personally don't mind waiting at my gate for a while, but part of the can be related to a trick my mom started teaching us when we were younger. Pack a power strip in your carry-on or personal bag. Most airlines give passengers a free carry on and personal item. I usually have a small suitcase for the carry on and a backpack or bag for my personal item. If you pack a power strip into one of the bag, you can have easy access to it. Then you only have to find one outlet and you group can all be close as you can plug many of your electronics in together and not be spread apart at many different outlets across the terminals.",
+		imgSrc_1: "images/airport_girls",
+		imgAlt_1: "Image of part of one of my travel groups together in the apirport, sitting with our legs proped up by our carry ons.",
+        imgSrc_2: "images/airplane_window",
+		imgAlt_2: "Image taken from the inside of an airplane through the window.",
+	},
+	{
+		id: 2,
+		title: "Cancun All Inclusive",
+		date: 'March 21, 2025',
+		sub_1: "Book in Advance and Look for Deals",
+		description_1:
+            "Just like how airports can get crazy at certain times and are bound to be more expensive and busier. Plan accordingly. Try your best to try and book in advance. This gives you more time to try and look for the best deals, make sure your able to book for your whole group, and to get cheaper prices. The closer you get to your travel date, it is often more and more expensive, so plan ahead, book ahead, and take time to look for any deals and discounts.",
+        sub_2: "Pros",
+        description2:
+            "One of the biggest pros my family enjoy when we did out all inclusive was the food. Easy access to drinks, snacks, buffets for meals, hotel restaurants, and late night room service. I still remember how fun it was when we had a little girls night, put on a fun show, and ordered up nachos, chocolate cake, and some sodas for a night with my mom and sister. Having such easy access to this and knowing it was already included in you bill can make the trip way easier. Depending on your pricing, it may have been less that what you would’ve spent at a different hotel where you had to pay for each thing. To consider this, think about how much you eat and drink, what kind of activities your wanting to do, and the location of the hotel. For my family, we spent most of our time playing and relaxing by the pool or on the beach, so we booked a hotel right on the beach that had a pool. We all snacked a lot and got many of the non-alcoholic drinks and stayed hydrated through out the day. Our resort had beach you could use such as chairs, that we set up and used on the beach. For us and that trip it was perfect.",
+        sub_3: "Cons",
+        description3:
+            "One con you may expect because it's all inclusive in an increase in price, now that may balance out if you spend your days relaxing at the resort and enjoying the amenities. However, if your plan is to go and site see, to explore and go about the city, it may not be the right fit as you'll still be buying food as your out and about. You might not be using all the free amenities that are costing you extra. If you plan to spend your day out in the city, on tours, in malls, on other beaches, then you will want to deeply consider if this is the right fit for your trip. ",
+		imgSrc_1: "images/cancun_pool",
+		imgAlt_1: "Image of the resort pool.",
+        imgSrc_2: "images/cuncun_kids",
+		imgAlt_2: "Image of part of my group, including me, my siblings, and a few of my cousins.",
+	},
+	{
+		id: 3,
+		title: "Disney Parks USA",
+		date: "March 22, 2025",
+		sub_1: "Be Mindful of Where You Stay",
+		description_1:
+            ": Like I've said on every post, be aware of busy times. For Disney there are usually an additional extra busy time, which is Halloween, so be aware of that in addition to the other very busy days. Now, with where you want to stay, that can depend on your family, but our most recent trip we have enjoyed staying at Disney resorts. In California and Disneyland, this is less important. If your at Disneyland, my advice would be to just stay really close. After you spent your day walking around, waiting in lines, standing, and enjoying your time, you will not want to walk a far distance away. My family (and me personally) really enjoys Marriot hotels, and there is a nice one that is positioned across from the park entrances and just up the street a little. It's a good option, specifically for groups and families. However in Florida, I would definitely encourage staying at a Disney resort. They are all super cutely themed, close to the parks, and give you access to specific transportation to the parks. My family has stayed at the two that are know as the budget ones, which are Art of Animation and Pop Century. These are both right by each other, separated by a lake, but connected at the Skyliners. They both have access to the Skyliners with a transfer and buses to the parks that don't have Skyliner access. This made getting to and from the parks pretty easy and quick and meant there was no parking fee and no waiting for a tram to get back to the car. It also made it really easy to park hop or to go back for lunch or a nap mid-day.",
+        sub_2: "Snacks and Reusable Water Bottles",
+        description2:
+            "Both California and Florida are very hot. To help keep your trip magical and save you a bit of money, go to a grocery store near you and get some easy to pack snacks into your park bag. I also always bring a reusable water bottle. There are lots of places where you can easily fill them up and keep yourself hydrated. If you want ice, you can always go and ask at one of the food locations for a cup of ice water too. If the regular reusable water bottle is to big for you, you can also reuse the smaller plastic ones you can buy too.",
+        sub_3: "Magicbands, Passes, and Lightning Lanes",
+        description3:
+            "While Magicbands are almost entire what is used in Walt Disney World in Florida, it wasn't quite as common the last time I went to Disneyland in California. It was still usable, but wasn't what a majority of the people were using. The Florida parks also have a lot more fun features with the Magicband+ that aren't in California, such as waving your band at specific status to make music play. These features can be fun for anyone, but only works with the Magicband+ not the standard Magicband. Magicbands can also act as your room key if you stay at a Disney Resort, for sure if Florida, this can really shrink check in time as you can check in with your phone, then get the room numbers on your phone and head straight up and open the doors with your charged Magicband. You can also connect it with a card and use it to pay for things in the park in Florida. It's always good to also be able to load up your passes on your phones as well. You can make a Disney account and have them connected on the My Disney Experience App. My family hasn't used the Lightning Lanes very often, instead, we try to plan and hit some of the typically bigger rides easy in the morning. Write kind of a game plan for what you really want to make sure you can do your best to do what you want to do. Lightning Lanes could be helpful if you don't want to wait in line for very long but be careful as sometime the Lightning Lanes end up longer than the regular line. Watch the wait times, and plan strategically where and when you want to ride what. Fireworks can be a great time to ride many popular rides, as well as early in the day, later at night, and during meal or nap times sometimes if your group doesn't need naps.",
+		imgSrc_1: "images/family_disney",
+		imgAlt_1: "Image of my family at Disneyland during the Christmas Season.",
+        imgSrc_2: "images/fam_dis_night",
+		imgAlt_2: "Image taken from the inside of an airplane through the window.",
+		},
+        {
+            id: 4,
+            title: "Cruises",
+            date: "March 22, 2025",
+            sub_1: "Book In Advance",
+		description_1:
+            "I feel like I aways need to emphasis this, but know the busy times. With cruises specifically though, you may find deals on the cruises when you book advance. It also gives you the benefit of more options for your room situation, choosing your deck, room style, room location. Think about how close do you want to be the elevator and stairs. Do you want a balcony, windows, or in inside room? What cruise line are you booking with? The earlier you book, the more room and excursions you have available to pick from.",
+        sub_2: "Drink and Internet Packages",
+        description2:
+            "Not everyone will agree with me, but I think the drink package in 100% a yes for me, and internet would be maybe 50-60% a yes. My family doesn't drink, but we love to hang around deck and get our sodas, mocktails, and fun drinks. It's nice to know it's already taken care of and that you won't have to pay for every single drink. Internet is nice to have, especially for communicating as you don't always have to be together. It's ok to split off when some people wanna swim, some wanna nap, and some wanna go and play trivia. There are usually plenty of options, so it's ok to be together for some of it, and buddied up for other parts. My family usually uses WhatsApp so we aren't charged a different fee on our phones for texting, but to have in work, you kind of need the internet package. The cruise lines we have traveled with have had different level of packaged that have costed different amounts, so I'd say for this, you just need to most basic one.",
+        sub_3: "Excursions",
+        description3:
+            "While there may be plenty of fun things on the boat, the cruise line has excursions that you can book as well. My family usually tries to book these for the port stops as since they are through the cruise line they tend to make sure you are back in time or early to make sure you make it back on the boat on time. You can look around and find other things and excursions at the port stops as well, but you'll need to carefully watch the time to make sure you are able to get back to the boat on time. ",
+		imgSrc_1: "images/cruise_girls",
+		imgAlt_1: "Image of my mom, my sister, and I on a cruise together.",
+        imgSrc_2: "images/curise_kids",
+		imgAlt_2: "Image taken fo my sibling, some of my cousins, and I from a cruise excursion together.",
+            }
+]
+
+const container = document.querySelector("#blog_posts");
+
+blog.forEach(function(item){
+
+	let book = document.createElement("blog");
+
+	let html =`
+	<div class="blog_post">
+        <h2>${item.title}</h2>
+		<p>${item.date}</p>
+		<p>${item.sub_1}</p>
+        <p>${item.description1}</p>
+        <p>${item.sub_2}</p>
+        <p>${item.description2}</p>
+        <p>${item.sub_3}</p>
+        <p>${item.description3}</p>
+		<img src="${item.imgSrc_1}" alt="${item.imgAlt_1}">
+        <img src="${item.imgSrc_2}" alt="${item.imgAlt_2}">
+		</div>`
+
+	book.innerHTML = html;
+
+	container.appendChild(book);
+
+});
+
+selectElem.addEventListener("change", changeTheme);
+
+function changeTheme() {
+    let current = selectElem.value;
+    console.log(current);
+    if (current == "dark") {
+        document.body.className = "dark";
+    } else {
+        document.body.classList.remove("dark");
+
+}
+}
